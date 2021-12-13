b5df80beb60b428abb0ab51a6e6122f68982b792
JorgeAndaur <aenimalabs@gmail.com>
2021-12-13 18:06:58 -0300

    con menu principal más volver

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'mains/index'
   # get 'departamentos/index'
   # get 'departamentos/show'
   # get 'departamentos/new'
@@ -12,6 +13,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # resources :edificios
+
+  root 'mains#index'
+
+  resources :mains
 
   resources :edificios do
     member do
